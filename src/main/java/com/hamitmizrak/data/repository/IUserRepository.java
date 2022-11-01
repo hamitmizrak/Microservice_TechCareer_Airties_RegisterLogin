b229a821efc0b31eee670cb6b716e7ee089573b6
hamitmizrak <hamitmizrak@gmail.com>
2022-11-01 20:18:45 +0300

    Initial commit

---
+++ src/main/java/com/hamitmizrak/data/repository/IUserRepository.java
@@ -0,0 +1,14 @@
+package com.hamitmizrak.data.repository;
+
+import com.hamitmizrak.data.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface IUserRepository extends JpaRepository<UserEntity,Long> {
+
+    //delivered query
+   Optional<UserEntity> findByUsername(String username);
+}
